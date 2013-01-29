61c4d92946dfbc7ed1990748889ae5eaabf25fb9
Armon Dadgar <armon.dadgar@gmail.com>
2013-01-29 14:34:23 -0800

    Start the acceptors and connection manager in bloomd_sup

--- src/bloomd_sup.erl
+++ src/bloomd_sup.erl
@@ -20,10 +20,26 @@ start_link() ->
 %% ===================================================================
 
 init(_Args) ->
+    % Collect all the variables first
+    {ok, Port} = application:get_env(bloomd_ascii_port),
+    {ok, AcceptPool} = application:get_env(bloomd_accept_pool),
+
+    % Connection Manager
+    ConnManager = {conn_manager,
+           {bloomd_conn_manager_sup, start_link, []},
+           permanent, 60000, supervisor, dynamic},
+
+    % Accept Manager, needs port and pool size
+    AcceptManager  = {acceptors,
+           {bloomd_acceptor_sup, start_link, [Port, AcceptPool]},
+           permanent, 60000, supervisor, dynamic},
+
+
     VMaster = { bloomd_vnode_master,
                   {riak_core_vnode_master, start_link, [bloomd_vnode]},
                   permanent, 5000, worker, [riak_core_vnode_master]},
 
     { ok,
         { {one_for_one, 5, 10},
-          [VMaster]}}.
+          [ConnManager, AcceptManager, VMaster]}}.
+
