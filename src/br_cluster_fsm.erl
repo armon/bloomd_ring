ee0accabd930213fd895e379acdd131060df78eb
Armon Dadgar <armon.dadgar@gmail.com>
2013-02-18 14:30:21 -0800

    First pass at full cluster fsm

--- src/br_cluster_fsm.erl
+++ src/br_cluster_fsm.erl
@@ -1,6 +1,6 @@
 -module(br_cluster_fsm).
 -behavior(gen_fsm).
--export([start_link/3]).
+-export([start_link/4]).
 -export([init/1, handle_info/3, handle_event/3, handle_sync_event/4,
          code_change/4, terminate/3]).
 -export([prepare/2, executing/2, waiting/2]).
@@ -12,23 +12,33 @@
         % Client PID to reply to
         from,
         % Operation
-        op
+        op,
+        % Operation args
+        args,
+        % Preflsit
+        preflist,
+        % Responses
+        resp=[]
         }).
+
+% This is the maximum time we wait
+% before we timeout the request.
+-define(WAIT_TIMEOUT, 60000).
 
 
 %%%
 % Start API
 %%%
 
-start_link(ReqId, From, Op) ->
-    gen_fsm:start_link(?MODULE, [ReqId, From, Op], []).
+start_link(ReqId, From, Op, Args) ->
+    gen_fsm:start_link(?MODULE, [ReqId, From, Op, Args], []).
 
 %%%
 % Gen FSM API
 %%%
 
-init([ReqId, From, Op]) ->
-    State = #state{req_id=ReqId, from=From, op=Op},
+init([ReqId, From, Op, Args]) ->
+    State = #state{req_id=ReqId, from=From, op=Op, args=Args},
     {ok, prepare, State, 0}.
 
 handle_info(Info, StateName, State) ->
@@ -54,12 +64,45 @@ terminate(_Reason, _StateName, _State) ->
 % FSM States
 %%%
 
-prepare(_Event, State) ->
-    {next_state, execute, State, 0}.
+% Prepares for execution, gets the preflist ready
+prepare(timeout, State) ->
+    Preflist = riak_core_apl:active_owners(bloomd),
+    {next_state, execute, State#state{preflist=Preflist}, 0}.
 
-executing(_Event, State) ->
-    {next_state, waiting, State, 0}.
 
-waiting(_Event, State) ->
-    {next_state, waiting, State, 0}.
+-define(MASTER, br_vnode_master).
+executing(timeout, State=#state{preflist=Pref, op=Op, args=Args}) ->
+    Cmd = case Op of
+        create ->
+            {Filter, Options} = Args,
+            {create_filter, Filter, Options};
+        drop -> {drop_filter, Args};
+        close -> {close_filter, Args};
+        clear -> {clear_filter, Args};
+        flush -> {flush_filter, Args};
+        info -> {info_filter, Args}
+    end,
+    riak_core_vnode_master:command(Pref, Cmd, {fsm, undefined, self()}, ?MASTER),
+    {next_state, waiting, State, ?WAIT_TIMEOUT}.
+
+
+waiting(timeout, State=#state{req_id=ReqId, from=From}) ->
+    lager:warning("Timed out waiting for all responses!"),
+    From ! {ReqId, timeout},
+    {stop, normal, State};
+
+waiting(Resp, State=#state{preflist=Pref, resp=Buf}) ->
+    NewBuf = [Resp | Buf],
+    NumResp = length(NewBuf),
+    Expect = length(Pref),
+    case NumResp of
+        % Have all the responses, we are done
+        Expect ->
+            #state{from=From, req_id=ReqId} = State,
+            From ! {ReqId, {ok, NewBuf}},
+            {stop, normal, State};
+
+        % Still waiting
+        _ -> {next_state, waiting, State#state{resp=NewBuf}, ?WAIT_TIMEOUT}
+    end.
 
