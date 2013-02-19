6597b62e23e4255e8f715eccfde0cb960884ce6d
Armon Dadgar <armon.dadgar@gmail.com>
2013-02-19 14:12:24 -0800

    Adding more utility methods

--- src/br_util.erl
+++ src/br_util.erl
@@ -1,6 +1,8 @@
 -module(br_util).
--export([ceiling/1]).
-
+-export([ceiling/1,
+         num_partitions/0,
+         keyslice/1,
+         keyslice/2]).
 
 % Implements the mathematical ceiling operator
 ceiling(X) when X < 0 ->
@@ -11,4 +13,25 @@ ceiling(X) ->
         true -> T;
         false -> T + 1
     end.
+
+% Returns the number of partitions
+num_partitions() ->
+    {ok, R} = riak_core_ring_manager:get_my_ring(),
+    riak_core_ring:num_partitions(R).
+
+
+% Gets the slice for a given key
+keyslice(Key) ->
+    P = num_partitions(),
+    keyslice(Key, P).
+
+
+% Given a key and the number of partitions, returns which
+% slice owns the given key
+keyslice(Key, Partitions) ->
+    % Get a SHA1 digest
+    <<Digest:160/integer>> = crypto:sha(term_to_binary(Key)),
+
+    % Mod by the ring size
+    Digest rem Partitions.
 
