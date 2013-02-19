5f95e8090129fa1fbf94e4e0b4c694fec5111dd3
Armon Dadgar <armon.dadgar@gmail.com>
2013-02-18 16:02:56 -0800

    File with utility methods

---
+++ src/br_util.erl
@@ -0,0 +1,14 @@
+-module(br_util).
+-export([ceiling/1]).
+
+
+% Implements the mathematical ceiling operator
+ceiling(X) when X < 0 ->
+    trunc(X);
+ceiling(X) ->
+    T = trunc(X),
+    case X - T == 0 of
+        true -> T;
+        false -> T + 1
+    end.
+
