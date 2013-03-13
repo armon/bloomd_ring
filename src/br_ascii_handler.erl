79405ab886787a4169bc8e56f4c442e304d0ded9
Armon Dadgar <armon.dadgar@gmail.com>
2013-03-12 17:20:17 -0700

    Enable no dealy, add recv and send buffer sizes

--- src/br_ascii_handler.erl
+++ src/br_ascii_handler.erl
@@ -30,6 +30,8 @@
 -define(NO_RESP, <<"No\n">>).
 -define(VALID_FILT_RE, "^[^ \t\n\r]{1,200}$").
 
+% Size of our send and receive buffers
+-define(BUF_SIZE, 65536).
 
 -ifdef(TEST).
 -compile(export_all).
@@ -52,7 +54,7 @@ handle_call(Msg, _From, State) ->
 
 % Wait for the start message, set the socket to active
 handle_cast(start, S=#state{socket=Socket}) ->
-    inet:setopts(Socket, [{active, true}]),
+    inet:setopts(Socket, [{active, true}, {nodelay, true}, {recbuf, ?BUF_SIZE}, {sndbuf, ?BUF_SIZE}]),
     {noreply, S}.
 
 
