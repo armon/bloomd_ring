46781bbaa96eef94720526c443369e395a925404
Armon Dadgar <armon.dadgar@gmail.com>
2013-03-14 13:17:55 -0700

    Start a poolboy pool of quorum FSM nodes

--- src/br_quorum_fsm_sup.erl
+++ src/br_quorum_fsm_sup.erl
@@ -10,11 +10,18 @@ start_link() ->
 
 
 init([]) ->
-    QuorumFSM = {undefined,
-                  {br_quorum_fsm, start_link, []},
-                  temporary, 5000, worker, [br_quorum_fsm]},
+    % Get the size of the pool
+    {ok, PoolSize} = application:get_env(bloomd_quorum_pool_size),
 
-    {ok, {{simple_one_for_one, 10, 10}, [QuorumFSM]}}.
+    % Start the worker pool
+    PoolArgs = [{name, {local, quorum_pool}},
+                {size, PoolSize},
+                {worker_module, br_quorum_fsm},
+                {max_overflow, 0}
+               ],
+    PoolSpec = poolboy:child_spec(quorum_pool, PoolArgs),
+
+    {ok, {{one_for_one, 10, 10}, [PoolSpec]}}.
 
 start_fsm(Args) ->
     supervisor:start_child(?MODULE, Args).
